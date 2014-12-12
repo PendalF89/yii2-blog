1fc1ce94a652ec3cfb7ff0721b120e89d8bbb58f
PendalF <pendalf@mail.ru>
2014-12-12 16:25:49 +0400

    Add some new methods for select posts

--- models/PostQuery.php
+++ models/PostQuery.php
@@ -12,7 +12,45 @@ class PostQuery extends ActiveQuery
      */
     public function published()
     {
-        $this->andWhere(['publish_status' => Post::STATUS_PUBLISHED]);
+        $this->andWhere(['blog_post.publish_status' => Post::STATUS_PUBLISHED]);
+        return $this;
+    }
+
+    /**
+     * Posts by type
+     *
+     * @param string $type
+     * @return $this
+     */
+    public function type($type)
+    {
+        $this->joinWith(['type']);
+        $this->andWhere(['blog_type.alias' => $type]);
+        return $this;
+    }
+
+    /**
+     * Posts by category
+     *
+     * @param string $category
+     * @return $this
+     */
+    public function category($category)
+    {
+        $this->joinWith(['category']);
+        $this->andWhere(['blog_category.alias' => $category]);
+        return $this;
+    }
+
+    /**
+     * Posts by alias
+     *
+     * @param string $alias
+     * @return $this
+     */
+    public function alias($alias)
+    {
+        $this->andWhere(['blog_post.alias' => $alias]);
         return $this;
     }
 }
